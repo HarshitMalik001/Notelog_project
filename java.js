a867878f630b7449240880adcdc53d0946a6438b
HarshitMalik001 <122423686+HarshitMalik001@users.noreply.github.com>
2023-05-22 14:24:49 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,20 +1,16 @@
-// Wait for the DOM to load
 document.addEventListener("DOMContentLoaded", function () {
     // Get a reference to the form and note list elements
     const form = document.querySelector("form");
     const noteList = document.getElementById("note-list");
   
-    // Add an event listener to the form's submit event
     form.addEventListener("submit", function (event) {
       event.preventDefault(); // Prevent the default form submission
   
-      // Get the values from the form inputs
       const title = document.getElementById("Notetitle").value;
       const date = document.getElementById("date").value;
       const time = document.getElementById("time").value;
       const note = document.getElementById("note").value;
   
-      // Create a new note element
       const noteElement = document.createElement("div");
       noteElement.className = "note";
       noteElement.innerHTML = `
@@ -24,10 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
         <p>${note}</p>
       `;
   
-      // Append the new note to the note list
       noteList.appendChild(noteElement);
   
-      // Reset the form
       form.reset();
     });
   });
